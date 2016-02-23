cfd6528b1e1b1af3256ef2eaf9ee6fdca1bc8216
Steve Withers <SteveWithers3@gmail.com>
2016-02-23 13:25:21 +0000

    Added range sweep file for collecting range data multithreaded for each ranger Added rangeData object for the return array

---
+++ Ultrasonics/RangeData.py
@@ -0,0 +1,4 @@
+class RangeData:
+  def __init__(self,angle,distance):
+    self.angle = angle
+    self.distance = distance
--- Ultrasonics/SR04_test.py
+++ Ultrasonics/SR04_test.py
@@ -1,9 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import numpy
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23 
 ECHO = 24
+
+READINGS = 21
+
+readings = numpy.zeros(READINGS)
 
 print "Distance Measurement In Progress"
 
@@ -14,22 +19,26 @@ GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+while True:
+  for j in range(0,READINGS- 1):
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+    while GPIO.input(ECHO)==0:
+      pulse_start = time.time()  #sometimes gets stuck here
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+    while GPIO.input(ECHO)==1:
+      pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+    pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
+    #distance = pulse_duration * 17150
 
-distance = round(distance, 2)
+    readings[j] = pulse_duration * 17150
+    #distance = round(distance, 2)
 
-print "Distance:",distance,"cm"
+    #print "Distance:",distance,"cm"
 
+  print "Distance: ",numpy.median(readings)," cm"
 GPIO.cleanup()
