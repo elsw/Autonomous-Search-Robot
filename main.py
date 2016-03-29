dceac99f0354dda3e9f6fc7e67f0d6781aea86d6
Steve Withers <SteveWithers3@gmail.com>
2016-03-29 13:11:05 +0000

    Changed main.py to fix pygame errors and moved pygame init to main

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from Ultrasonics.RangeSweep import RangeSweep
 from Ultrasonics.StepSweep import StepSweep
 from Ultrasonics.RangeData import RangeData
 from Mapping.Mapping import Mapping
+from Mapping.Navigation import Navigation
+from Motor_Driver.MotorDriver import MotorDriver
 import time
 import pygame
+from pygame.locals import *
 import RPi.GPIO as GPIO
 
 
@@ -13,27 +16,41 @@ if __name__ == "__main__":
     pwm = PWM(0x40)
     pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
     #pwm = PWM(0x40, debug=True)
-    m = Mapping();
+    screen = pygame.display.set_mode((700, 700))
+    m = Mapping(screen);
     print "setting up rangers"
     r = StepSweep(pwm)
+    motor = MotorDriver()
+    nav = Navigation(motor,screen)
 
     running = True
     wait_for_input = True
     
     print "reading..."
     while running:
-        wait_for_input = True
         r.servoToStart()
         time.sleep(1)
         rangeData = r.fullRange()
         m.rawMap(rangeData)
+        nav.drive(rangeData)
 
-        while wait_for_input:
-            events = pygame.event.get()
-            for event in events:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        running = False
-                        wait_for_input = False
+        pygame.display.flip()
+
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                wait_for_input = False
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    wait_for_input = True
+                    while wait_for_input:
+                        for event in pygame.event.get():
+                            if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_SPACE:
+                                    wait_for_input = False
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+    r.cleanup()
     pygame.quit()
     GPIO.cleanup()
