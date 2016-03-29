bff44708bc650a0600cad00e6acabefd0a193852
Steve Withers <SteveWithers3@gmail.com>
2016-03-29 12:54:56 +0000

    moved Mapping.py to single_mapping.py

---
+++ Mapping/single_map.py
@@ -0,0 +1,26 @@
+from Ultrasonics.RangeData import RangeData
+import pygame
+import math
+
+class Mapping:
+    def __init__(self):
+        pygame.display.init()
+        self.screen = pygame.display.set_mode((500, 500))
+        self.locX = 250
+        self.locY = 250
+
+    def rawMap(self,rangeData):
+        self.screen.fill((255,255,255))
+        pygame.draw.circle(self.screen,(0,0,255),(self.locX,self.locY), 5,0)
+        pygame.draw.circle(self.screen,(255,0,0),(self.locX,self.locY), 100, 1)
+        pygame.draw.circle(self.screen,(255,0,0),(self.locX,self.locY), 200, 1)
+        for i in range(0,len(rangeData) - 1):
+            angle = rangeData[i].angle - 90
+            x = rangeData[i].distance *1* math.cos(math.radians(angle))
+            y = rangeData[i].distance *1* math.sin(math.radians(angle))
+            self.__drawDot(self.locX + int(x),self.locY + int(y))
+        pygame.display.flip()
+
+    def __drawDot(self,x,y):
+        pygame.draw.circle(self.screen,(0,0,0),(x,y), 5,0)
+        
