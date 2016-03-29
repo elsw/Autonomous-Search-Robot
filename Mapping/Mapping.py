086e689cf136b0f443311616ab88ed0ca08266d1
Steve Withers <SteveWithers3@gmail.com>
2016-03-29 15:05:52 +0000

    Fixing various bugs

--- Mapping/Mapping.py
+++ Mapping/Mapping.py
@@ -4,52 +4,60 @@ import math
 
 class Mapping:
     def __init__(self,screen):
-        pygame.display.init()
         self.screen = screen
         self.locX = 350
         self.locY = 350
         self.rotation = 0
         self.cmPerPix = 1
         self.gap_data = []
-        self.rangeData = []
+        self.vertexData = []
         
     def addRangeData(self,rangeData):
-        self.rangeData.extend(rangeData)
+        for r in rangeData:
+            angle = r.angle - 90 + self.rotation
+            x = self.locX + r.distance *self.cmPerPix* math.cos(math.radians(angle))
+            y = self.locY + r.distance *self.cmPerPix* math.sin(math.radians(angle))
+            self.vertexData.append(Vertex(int(x),int(y))) #add vertex with default probability
     
-    def draw(self):
-        self.screen.fill((255,255,255))
-        self.__drawMap(self.rangeData)
+    def drawVertex(self):
+        
+        self.__drawMap()
 
     def getPosition(self):
         return (self.locX,self.locY)
 
     def updatePosition(self,movedRotDis):
-        self.locX = self.locX + movedRotDis[1] *self.cmPerPix* math.cos(math.radians(movedRotDis[0]))
-        self.locY = self.locY + movedRotDis[1] *self.cmPerPix* math.sin(math.radians(movedRotDis[0]))
+        rot_cm = movedRotDis[1] * 100 #convert m to cm
         self.rotation = self.rotation + movedRotDis[0]
         if self.rotation > 360:
             self.rotation = self.rotation - 360
         if self.rotation < 0:
             self.rotation = self.rotation + 360
-
+            
+        self.locX = self.locX + rot_cm *self.cmPerPix* math.cos(math.radians(self.rotation - 90))
+        self.locY = self.locY + rot_cm *self.cmPerPix* math.sin(math.radians(self.rotation - 90))
+        
+    def drawPosition(self):
+        pygame.draw.circle(self.screen,(0,0,255),(int(self.locX),int(self.locY)), 5,0)
+        lineX = self.locX + 15 * math.sin(math.radians(self.rotation))
+        lineY = self.locY + 15 * math.sin(math.radians(self.rotation))
+        pygame.draw.line(self.screen,(0,0,255),(int(self.locX),int(self.locY)),(int(lineX),int(lineY)),3)
     
-    def __drawMap(self,rangeData):
-        pygame.draw.circle(self.screen,(0,0,255),(self.locX,self.locY), 5,0)
-        pygame.draw.circle(self.screen,(255,0,0),(self.locX,self.locY), 100 *self.cmPerPix, 1)
-        pygame.draw.circle(self.screen,(255,0,0),(self.locX,self.locY), 200 *self.cmPerPix, 1)
-        for r in rangeData:
-            angle = r.angle - 90
-            x = r.distance *self.cmPerPix* math.cos(math.radians(angle))
-            y = r.distance *self.cmPerPix* math.sin(math.radians(angle))
-            self.__drawDot(self.locX + int(x),self.locY + int(y))
+    def drawVertex(self):
+        pygame.draw.circle(self.screen,(255,0,0),(int(self.locX),int(self.locY)), 100 *self.cmPerPix, 1)
+        pygame.draw.circle(self.screen,(255,0,0),(int(self.locX),int(self.locY)), 200 *self.cmPerPix, 1)
+
+        for vertex in self.vertexData:
+            self.__drawDot(vertex.getX(),vertex.getY())
     
     def __drawDot(self,x,y):
         pygame.draw.circle(self.screen,(0,0,0),(x,y), 5,0)
         
-class Vertex(self,x,y,p = 0.5):
-    self.x = x
-    self.y = y
-    self.p = p
+class Vertex:
+    def __init__(self,x,y,p = 0.5):
+        self.x = x
+        self.y = y
+        self.p = p
 
 
     def getX(self):
