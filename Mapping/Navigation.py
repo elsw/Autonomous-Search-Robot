fa053e588514704084f4da2ec101addc199ae45e
Steve Withers <SteveWithers3@gmail.com>
2016-03-29 13:12:28 +0000

    add new file used to control the motors using the range map by calculating the gaps

---
+++ Mapping/Navigation.py
@@ -0,0 +1,103 @@
+from Motor_Driver.MotorDriver import MotorDriver
+import pygame
+import math
+
+class Navigation:
+    def __init__(self,motorDriver,screen):
+        self.motorDriver = motorDriver
+        self.screen = screen        
+        self.driveStep = 0.5#aim for driving 0.5 meter berfore anther range map
+        self.gap_data = []
+        self.readLimit = 200 #ignore everything over 200 cm
+        self.locX = 350
+        self.locY = 350
+
+
+    def drive(self,rangeData):
+        self.__calculateGaps(rangeData,True)
+        if len(self.gap_data) > 0:
+            gap = self.__chooseGap(self.gap_data)
+            self.motorDriver.left(-gap.getCenterAngle())
+            self.motorDriver.forward(0.5)
+            
+
+
+    def __calculateGaps(self,rangeData, debug = False):
+        minGapAngle = 20 #at least 20 degrees to be considered a gap
+        self.gap_data = []
+        lastAngle = 0.0
+        lastIndex = 0
+        #find last angle before 360
+        for n in range(0, len(rangeData)):
+            i = -n + (len(rangeData) - 1)
+            if(rangeData[i].distance < self.readLimit):
+                lastAngle = rangeData[i].angle - 360
+                lastIndex = i
+                break
+
+        #run through all points finding gaps
+        for i in range(0,len(rangeData)):
+            if rangeData[i].distance < self.readLimit:
+                if rangeData[i].angle - lastAngle > minGapAngle:
+                    self.gap_data.append(GapData(rangeData[lastIndex],rangeData[i],self.locX,self.locY))
+                lastAngle = rangeData[i].angle
+                lastIndex = i
+        if(debug):
+            for gap in self.gap_data:
+                pygame.draw.line(self.screen,[0,255,0],gap.getLeftPos(),gap.getRightPos(),2)
+                
+
+    def __chooseGap(self,gapData):
+        bestIndex = 0
+        bestAngle = 10000
+        #just choose most nearest facing atm
+        for i in range(0,len(gapData)):
+            if abs(gapData[i].getCenterAngle()) < bestAngle:
+                bestAngle = gapData[i].getCenterAngle()
+                bestIndex = i
+        return gapData[bestIndex]
+
+class GapData:
+    def __init__(self,rangeDataLeft,rangeDataRight,locX = 0, locY = 0, cmPerPix = 1):
+        self.rangeDataLeft = rangeDataLeft
+        self.rangeDataRight = rangeDataRight
+        self.cmPerPix = cmPerPix
+        #optional position of robot
+        self.locX = locX
+        self.locY = locY
+
+    def getLeftPos(self):                                              
+        pos = []
+        # calculate X
+        pos.append(self.locX + int(self.rangeDataLeft.distance *self.cmPerPix* math.cos(math.radians(self.rangeDataLeft.angle - 90))))
+        #calculate Y
+        pos.append(self.locY + int(self.rangeDataLeft.distance *self.cmPerPix* math.sin(math.radians(self.rangeDataLeft.angle - 90))))
+        return pos
+
+    def getRightPos(self):                                              
+        pos = []
+        # calculate X
+        pos.append(self.locX + int(self.rangeDataRight.distance *self.cmPerPix* math.cos(math.radians(self.rangeDataRight.angle - 90))))
+        #calculate Y
+        pos.append(self.locY + int(self.rangeDataRight.distance *self.cmPerPix* math.sin(math.radians(self.rangeDataRight.angle - 90))))
+        return pos
+
+                                                                        
+    def getCenterAngle(self):
+        left = 0.0
+        right = 0.0
+        if self.rangeDataRight.angle - self.rangeDataLeft.angle < 0:
+            #in this case the gap is over the 360 boundry
+            right = self.rangeDataRight.angle
+            left = self.rangeDataLeft.angle - 360
+        else:
+            #other wise not over the 360 boundry
+            right = self.rangeDataRight.angle
+            left = self.rangeDataLeft.angle
+        return right - ((right - left)/2)
+
+    def getGapSize(self):
+        left = self.getLeftPos()
+        right = self.getRightPos()
+        return sqrt(sqr(right[0] - left[0]) + sqr(right[1] - left[1]))
+        
