be4abbea9710ccc590eadd633e9782c04a04f506
Steve Withers <SteveWithers3@gmail.com>
2016-03-29 15:45:18 +0000

    Changed main test sequence

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from Servo_Driver.Adafruit_PWM_Servo_Driver import PWM
 from Ultrasonics.RangeSweep import RangeSweep
+from Ultrasonics.StepSweep import StepSweep
+from Ultrasonics.RangeData import RangeData
 from Mapping.Mapping import Mapping
 import time
 import pygame
+import RPi.GPIO as GPIO
 
-running = True
+
 
 if __name__ == "__main__":
     pwm = PWM(0x40)
@@ -12,21 +15,25 @@ if __name__ == "__main__":
     #pwm = PWM(0x40, debug=True)
     m = Mapping();
     print "setting up rangers"
-    r = RangeSweep(pwm)
+    r = StepSweep(pwm)
+
+    running = True
+    wait_for_input = True
     
     print "reading..."
     while running:
+        wait_for_input = True
         r.servoToStart()
         time.sleep(1)
-        data = r.fullRange()
-        m.rawMap(data)
-        
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    running = False
-    
-    #raw_input()
+        rangeData = r.fullRange()
+        m.rawMap(rangeData)
+
+        while wait_for_input:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        running = False
+                        wait_for_input = False
     pygame.quit()
-    r.cleanup()
+    GPIO.cleanup()
