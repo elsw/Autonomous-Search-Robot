af0e577871e082d5996acccc66cb6ef736868688
Steve Withers <SteveWithers3@gmail.com>
2016-04-21 13:10:32 +0000

    fixed servo to start to use the new servo driver

--- ServoToStart.py
+++ ServoToStart.py
@@ -1,12 +1,14 @@
 from Servo_Driver.Adafruit_PWM_Servo_Driver import PWM
-from Ultrasonics.RangeSweep import RangeSweep
+from Ultrasonics.StepSweep import StepSweep
+from Servo_Driver.ServoDriver import ServoDriver
 import time
 
 if __name__ == "__main__":
-    pwm = PWM(0x40)
-    pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+    #pwm = PWM(0x40)
+    #pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
     #pwm = PWM(0x40, debug=True)
-    r = RangeSweep(pwm)
+    servoDriver = ServoDriver()
+    r = StepSweep(servoDriver)
     r.servoToStart()
     time.sleep(0.5)
     r.cleanup()
