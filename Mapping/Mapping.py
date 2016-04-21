de23c65a7cede640a67a7a949d89348469faba40
Steve Withers <SteveWithers3@gmail.com>
2016-04-21 14:50:26 +0000

    changed from data fusion mapping to basic priopablity based mapping

--- Mapping/Mapping.py
+++ Mapping/Mapping.py
@@ -7,13 +7,17 @@ class Mapping:
         self.screen = screen
         self.locX = 350
         self.locY = 350
+        self.lastLocX = 350
+        self.lastLocY = 350
         self.rotation = 0
+        self.lastRotation = 0
         self.cmPerPix = 1
         self.gap_data = []
         self.vertexData = []
         self.path = []
         #first path point is always start point
         self.path.append(PathPoint(self.locX,self.locY))
+        self.enableP = True
 
         
         self.rangeLimit = 200 #this is the max range that readings are tacken as true
@@ -31,16 +35,27 @@ class Mapping:
 
     def getPosition(self):
         return (self.locX,self.locY)
+    def getLastPosition(self):
+        return (self.lastLocX,self.lastLocY)
     def getRotation(self):
         return self.rotation
+    def getLastRotation(self):
+        return self.lastRotation
+
+    def toggleEnableP(self):
+        self.enableP = not self.enableP
 
     def updatePosition(self,movedRotDis):
         rot_cm = movedRotDis[1] * 100 #convert m to cm
+        self.lastRotation = self.rotation
         self.rotation = self.rotation + movedRotDis[0]
         if self.rotation > 360:
             self.rotation = self.rotation - 360
         if self.rotation < 0:
             self.rotation = self.rotation + 360
+
+        self.lastLocX = self.locX
+        self.lastLocY = self.locY
             
         self.locX = self.locX + rot_cm *self.cmPerPix* math.cos(math.radians(self.rotation - 90))
         self.locY = self.locY + rot_cm *self.cmPerPix* math.sin(math.radians(self.rotation - 90))
@@ -61,11 +76,14 @@ class Mapping:
         pygame.draw.circle(self.screen,(255,0,0),(int(self.locX),int(self.locY)), 200 *self.cmPerPix, 1)
 
         for vertex in self.vertexData:
-            self.__drawDot(vertex)
+            self.__drawDot(vertex,self.enableP)
     
-    def __drawDot(self,vertex):
+    def __drawDot(self,vertex,enableP):
         p = int(255 - (vertex.getP() * 255))
-        pygame.draw.circle(self.screen,(p,p,p),(vertex.getX(),vertex.getY()), 5,0)
+        if enableP:
+            pygame.draw.circle(self.screen,(p,p,p),(vertex.getX(),vertex.getY()), 5,0)
+        else:
+            pygame.draw.circle(self.screen,(125,125,125),(vertex.getX(),vertex.getY()), 5,0)
 
     #this function adds a new vertex and increases the propability of are vertex that are nearby
     def __addVertex(self,vertex):
@@ -86,44 +104,20 @@ class Mapping:
                     if j != i:
                         distance = self.vertexData[j].distanceTo(self.vertexData[i])
                         if distance < self.agreeRange:
-                            #fuse vertices
-                            #interpolate new vertex depending on propability difference
-                            #pInterpolate is between 0 and 1, 0.5 being both the same P
-                            pInt = (0.5 + (self.vertexData[i].getP() - self.vertexData[j].getP())/2)
-                            vXi = self.vertexData[i].getX()
-                            vYi = self.vertexData[i].getY()
-                            vXj = self.vertexData[i].getX()
-                            vYj = self.vertexData[i].getY()
-                            newX = vXi + ((vXj - vXi)* pInt)
-                            newY = vYi + ((vYj - vYi)* pInt)
-                            newP = 0.5
-                            if self.vertexData[i].getP() > self.vertexData[j].getP():
-                                newP = self.vertexData[i].getP()
+                            #increase P on matched vertex (capped at 1)
+                            if self.vertexData[j].getP() < 0.9:
+                                self.vertexData[j].setP(self.vertexData[j].getP() + 0.1)
                             else:
-                                newP = self.vertexData[j].getP()
-                            if newP < 0.8:
-                                newP = newP + 0.2
-                            else:
-                                newP = 1
-                            #remove both old vertices
-                            if i > j:
-                                self.vertexData.pop(i)
-                                self.vertexData.pop(j)
-                            else:
-                                self.vertexData.pop(j)
-                                self.vertexData.pop(i)
-                            #add new vertex
-                            self.vertexData.append(Vertex(int(newX),int(newY),int(newP)))
-                            #print "fuse"
-                            break
-                if inRejectRange == False:
-                    #print "none in range"
-                    #nothing was in reject range
-                    #reduce probabilty of being a good reading
-                    if self.vertexData[i].getP() > 2:
-                        self.vertexData[i].setP(self.vertexData[i].getP() - 0.2)
+                                self.vertexData[j].setP(1)
+                            #found a match so dont decrease P
+                            inAgreeRange = True
+                if inAgreeRange == False:
+                    #nothing was in agree range
+                    #reduce probabilty of being a good reading (capped to 0.1)
+                    if self.vertexData[i].getP() > 0.2:
+                        self.vertexData[i].setP(self.vertexData[i].getP() - 0.1)
                     else:
-                        self.vertexData[i].setP(0)
+                        self.vertexData[i].setP(0.1)
                 
 
 #Class that holds simple positional data and a probability number
